77c72ea41b2bb7934c9402bab32ab505e232f3ea
namukuri <namukuri@gmail.com>
2025-01-13 12:49:14 +0900

    手順７まで

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -13,12 +14,17 @@ public class PlayerController : MonoBehaviour
     private float horizontal;  // x 軸(水平・横)方向の入力の値の代入用
     private float vertical; //y 軸(垂直・縦)方向の入力の値の代入用
 
+    private Animator anim;  // コンポーネントの取得用
+
+    private Vector2 lookDirection = new Vector2(0f, -1.0f);  // キャラの向きの情報の設定用
 
     // Start is called before the first frame update
     void Start()
     {
         // このスクリプトがアタッチされているゲームオブジェクトにアタッチされているコンポーネントの中から、<指定>したコンポーネントの情報を取得して、左辺に用意した変数に代入
         rd = GetComponent<Rigidbody2D>(); // あるいは、TryGeyComponent(out rb);　でも可
+
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,6 +35,9 @@ public class PlayerController : MonoBehaviour
 
         // InputManager の Vertical に登録してあるキーが入力されたら、水平(横)方向の入力値として代入
         vertical = Input.GetAxis("Vertical");
+
+        // キャラの向いている方向と移動アニメの同期
+        SyncMoveAnimation();
     }
 
     private void FixedUpdate()
@@ -43,9 +52,38 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         // 斜め移動の距離が増えないように正規化処理を行い、単位ベクトルとする(方向の情報は持ちつつ、距離による速度差をなくして一定値にする)
-        Vector3 dir = new Vector3 (horizontal, vertical, 0).normalized;
+        Vector3 dir = new Vector3(horizontal, vertical, 0).normalized;
 
         // velocity(速度)に新しい値を代入して、ゲームオブジェクトを移動させる
         rd.velocity = dir * moveSpeed;
     }
+
+    // キャラの向いている方向と移動アニメの同期
+    private void SyncMoveAnimation()
+    {
+        // 移動のキー入力値を代入
+        Vector2 move = new Vector2(horizontal, vertical);
+
+        // いずれかのキー入力があるか確認
+        if(!Mathf.Approximately(move.x, 0.0f) || !Mathf.Approximately(move.y, 0.0f));
+        {
+
+        // 向いている方向を更新
+        lookDirection.Set(move.x, move.y);
+
+         // 正規化
+         lookDirection.Normalize();
+
+        // キー入力の値と Blend Tree で設定した移動アニメ用の値を確認し、移動アニメを再生
+        anim.SetFloat("Look X", lookDirection.x);
+        anim.SetFloat("Look Y", lookDirection.y);
+        }
+
+        // 停止アニメーション用
+        anim.SetFloat("Speed", move.magnitude);
+     }
 }
+
+
+
+
